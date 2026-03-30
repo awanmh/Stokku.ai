6b00699a75af869b60e4e29096c51776ae93a6c4
awanmh <setiawanmaulana6@gmail.com>
2026-03-30 13:23:32 +0700

    Save state

--- backend/migrations/000001_init.down.sql
+++
@@ -1,6 +0,0 @@
--- Rollback: Drop all tables
-DROP TABLE IF EXISTS transactions;
-DROP TABLE IF EXISTS stocks;
-DROP TABLE IF EXISTS warehouses;
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS users;
